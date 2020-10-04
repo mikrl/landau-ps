1bc5ac90dc411fcda9f35e6f4320ad68fbbbdae8
Michael <mikrlynch@gmail.com>
2020-10-04 13:19:26 -0400

    Added more tests

--- tests/tests.cxx
+++ tests/tests.cxx
@@ -5,7 +5,7 @@
 /** SimConfig Test
  * Tests the SimConfig struct
  */
-TEST_CASE("Test SimConfig", "[main]" ){
+TEST_CASE("Test SimConfig", "[simcfg]" ){
   SimConfig test_simconfig  = {"cylindrical", {0.0}, "standard", "cli"};
   REQUIRE ( test_simconfig.geometry == "cylindrical");
   REQUIRE ( test_simconfig.extent[0] == 0.0) ;
@@ -13,13 +13,33 @@ TEST_CASE("Test SimConfig", "[main]" ){
   REQUIRE ( test_simconfig.output == "cli");
 }
 
-TEST_CASE("Test", "[main]" ){
+TEST_CASE("Test FieldConfig", "[simcfg]" ){
 
+  FieldConfig test_fieldconfig = {} ;
+
+  REQUIRE ( test_simconfig.geometry == "cylindrical");
+  REQUIRE ( test_simconfig.extent[0] == 0.0) ;
+  REQUIRE ( test_simconfig.discretization == "standard");
+  REQUIRE ( test_simconfig.output == "cli");
+}
+
+TEST_CASE("Initialize and delete a Simulation object", "[initsim]" ){
+  SimConfig test_cfg;
+  Simulation testsim (test_cfg);
+  delete testsim;
   REQUIRE ( 0 );
 }
 
-TEST_CASE("Test", "[main]" ){
+TEST_CASE("Add a field", "[addfield]" ){
+  SimConfig test_cfg;
+  Simulation test_sim (test_cfg);
+  test_sim.AddField()
+  REQUIRE ( 0 );
+}
 
+TEST_CASE("Add a particle", "[addparticle]" ){
+  SimConfig test_cfg;
+  Simulation test_sim (test_cfg);
   REQUIRE ( 0 );
 }
 
