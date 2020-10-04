60f730f0ad7d56903c3fe5f227dfb484645783a8
Michael <mikrlynch@gmail.com>
2020-10-04 18:40:58 -0400

    Wrote more tests for initializing simulation

--- include/experiment.hxx
+++ include/experiment.hxx
@@ -11,59 +11,67 @@
 #include <memory>
 #include <string>
 
-
 //enum BoundaryConditions {periodic, fixed, free}
 
 struct SimConfig{
   std::string geometry;
   std::vector<float> extent;
   std::string discretization;
+  unsigned long max_cells;
   std::string output;
 } ;
 
-//!  A test class. 
+//!  The Simulation class
 /*!
-  A more elaborate class description.
+  Initializes the simulation object ready to add particles, fields, etc
 */
 class Simulation
 {
 private:
-  long t_step; 
-  SpatialVolume _free_space;  
-  std::vector<Field> _fields;
-  std::vector<Particle> _particles;
-  std::vector<Material> _materials;
-  std::vector<Force> _forces;
+  long t_step_; 
+  SpatialVolume free_space_;  
+  std::vector<Field> fields_;
+  std::vector<Particle*> particles_;
+  std::vector<Material> materials_;
+  std::vector<Force> forces_;
   
 public:
   /** Constructor.
-   * Takes a configuration as input and returns a pointer.
-   */
+   * Returns a simulation object with default configuration (euclidean, 10m x 10m x 10m, 1000 cells)
+   * Or takes a configuration as input and returns a pointer.
+   */ 
+  Simulation(); // default
   Simulation(const SimConfig configuration);
   /** Destructor.
    * Cleans up the Simulation object.
    */
   virtual ~Simulation();
   /** AddParticle.
-   * Takes a ParticleConfig as input and adds a new particle to the system.
+   * Adds a new particle to the system with r=v=a=0 (all vector quants) and no interactions
+   * Also can take a ParticleConfig as input to modify default values
    */
+  void AddParticle();
   void AddParticle(ParticleConfig new_particle_cfg);
   /** RemoveParticle.
    * Takes a particle ID as input and removes the particle from the system.
-   * Or takes the particle name as a string and does the same.
    */
-  void RemoveParticle(unsigned int particle_ID);
-  void RemoveParticle(std::string particle_name);
+  void RemoveParticle(std::string particle_ID);
   /** AddField.
    * Takes a FieldConfig as input and adds a new force field to the system.
    */
+  /** GetParticles.
+   * Returns a vector containing the names of all the particles
+   */
+  std::vector<std::string> GetParticleList();
+  /** GetConfiguration
+   * Reads the Simulation configuration and returns the struct
+   */
+  SimConfig GetConfiguration();
   void AddField(FieldConfig new_field_cfg);
   /** RemoveField.
    * Takes a field ID as input and removes the field from the system.
-   * Or takes the field name as a string and does the same.
    */
-  void RemoveField(unsigned int field_ID);
-  void RemoveField(std::string field_name);
+  void RemoveField(std::string field_ID);
 
   /** Solve.
    * Runs the solver and evolves the system according to active fields and forces
--- tests/tests.cxx
+++ tests/tests.cxx
@@ -2,45 +2,62 @@
 #include <catch2/catch.hpp>
 #include<experiment.hxx>
 
+#include <algorithm>
+
 /** SimConfig Test
  * Tests the SimConfig struct
  */
 TEST_CASE("Test SimConfig", "[simcfg]" ){
-  SimConfig test_simconfig  = {"cylindrical", {0.0}, "standard", "cli"};
+  SimConfig test_simconfig  = {"cylindrical", {0.0}, "standard", 100,  "cli"};
   REQUIRE ( test_simconfig.geometry == "cylindrical");
   REQUIRE ( test_simconfig.extent[0] == 0.0) ;
   REQUIRE ( test_simconfig.discretization == "standard");
   REQUIRE ( test_simconfig.output == "cli");
 }
 
-TEST_CASE("Test FieldConfig", "[simcfg]" ){
+// TEST_CASE("Test FieldConfig", "[simcfg]" ){
 
-  FieldConfig test_fieldconfig = {} ;
-
-  REQUIRE ( test_simconfig.geometry == "cylindrical");
-  REQUIRE ( test_simconfig.extent[0] == 0.0) ;
-  REQUIRE ( test_simconfig.discretization == "standard");
-  REQUIRE ( test_simconfig.output == "cli");
-}
+//   FieldConfig test_fieldconfig = {} ;
+  
+//   REQUIRE (1);
+// }
 
 TEST_CASE("Initialize and delete a Simulation object", "[initsim]" ){
-  SimConfig test_cfg;
-  Simulation testsim (test_cfg);
-  delete testsim;
-  REQUIRE ( 0 );
+  // SimConfig test_cfg;
+  Simulation* test_simulation = new Simulation  ();
+  auto test_cfg = test_simulation->GetConfiguration();
+    std::vector<float> test_extent {0.0, 10.0, 0.0, 10.0, 0.0, 10.0};
+    
+  REQUIRE(test_cfg.geometry == "euclidean");
+  REQUIRE(test_cfg.extent == test_extent);
+  REQUIRE(test_cfg.discretization == "standard");
+  REQUIRE(test_cfg.max_cells == 1000);
+  REQUIRE(test_cfg.output == "cli");
+  delete test_simulation;
+  //REQUIRE ( 1 );
 }
 
-TEST_CASE("Add a field", "[addfield]" ){
-  SimConfig test_cfg;
-  Simulation test_sim (test_cfg);
-  test_sim.AddField()
-  REQUIRE ( 0 );
-}
+// TEST_CASE("Add a field", "[addfield]" ){
+//   SimConfig test_cfg;
+//   Simulation test_sim (test_cfg);
+//   test_sim.AddField()
+//   REQUIRE ( 0 );
+// }
 
-TEST_CASE("Add a particle", "[addparticle]" ){
+TEST_CASE("Add and remove particles", "[addparticle]" ){
   SimConfig test_cfg;
   Simulation test_sim (test_cfg);
-  REQUIRE ( 0 );
+  auto particle_list = test_sim.GetParticleList();
+  REQUIRE ( particle_list.size() == 0 );
+
+  test_sim.AddParticle();
+  particle_list = test_sim.GetParticleList();
+  REQUIRE ( particle_list.size() == 1 );
+
+  auto particle_ID = particle_list[0];
+  test_sim.RemoveParticle(particle_ID);
+  particle_list = test_sim.GetParticleList();
+  REQUIRE ( particle_list.size() == 0 );  
 }
 
 TEST_CASE("Test", "[main]" ){
